bb1e31b73ee322ca7ea2091abc79efb68f543e98
nibras <nibrasnib1526@gmail.com>
2024-02-21 14:02:19 +0600

    basics

--- 01_basic-setup/tailwind.config.js
+++ 01_basic-setup/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        chestnut:'#973f29'
+      }
+    },
   },
   plugins: [],
 }
